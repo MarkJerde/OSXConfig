252d0d1a20a62c614d9fea1f107c8626dc238c1a
Mark Jerde <mark.jerde@hgst.com>
2015-09-09 15:25:45 -0500

    Make sure the link being created has a directory.

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ echo Library/Preferences/com.apple.Terminal.plist
 perl -e 'exit 0 if ('$(sw_vers -productVersion|sed 's/^\([0-9][0-9]*\.[0-9][0-9]*\).*/\1/')' < 10.8);exit 1;' ] && echo Library/Preferences/ByHost/com.apple.screensaver.HOST-UUID.plist # Screensavers changed in OS X 10.8, so only do this for 10.7 and less.
 ) ; do
 	echo Symbolic linking "$file"
+	mkdir -p $(dirname "$TO/$file")
 	ln -s "$FROM/$file" "$TO/$file"
 	ls -l "$TO/$file"
 done
