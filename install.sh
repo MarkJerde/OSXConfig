27b18eff53e9892f5a1dde0e6cd5a46ebda71a55
Mark Jerde <mark.a.jerde@gmail.com>
2022-04-29 11:39:00 -0500

    Configure trackpad

--- install.sh
+++ install.sh
@@ -121,6 +121,7 @@ echo Library/Preferences/com.apple.Terminal.plist
 perl -e 'exit 0 if ('$(sw_vers -productVersion|sed 's/^\([0-9][0-9]*\.[0-9][0-9]*\).*/\1/')' < 10.8);exit 1;' ] && echo Library/Preferences/ByHost/com.apple.screensaver.HOST-UUID.plist # Screensavers changed in OS X 10.8, so only do this for 10.7 and less.
 echo Library/Developer/Xcode/UserData/FontAndColorThemes
 echo Library/Application Support/Flycut/com.generalarcade.flycut.plist
+echo Library/Preferences/com.apple.AppleMultitouchTrackpad.plist
 ) ; do
 	tofile="$(echo "$file"|sed 's/HOST-UUID/'"$hostUUID"'/')"
 	echo Symbolic linking "$file"
